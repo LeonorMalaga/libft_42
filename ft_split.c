c22bd98156162bf4fc94f434cc7a6d42fd0ca24b
Leonor Martinez Mesas <leonmart@c3r6s3.42malaga.com>
2024-04-27 20:34:12 +0200

    norminette

--- ft_split.c
+++ ft_split.c
@@ -6,20 +6,20 @@
 /*   By: leonmart <leonmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 15:35:03 by leonmart          #+#    #+#             */
-/*   Updated: 2024/04/27 19:05:58 by leonmart         ###   ########.fr       */
+/*   Updated: 2024/04/27 19:29:33 by leonmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void free_array(char **s2)
+static void	free_array(char **s2)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	if(s2)
+	if (s2)
 	{
-		while(s2[i])
+		while (s2[i])
 		{
 			free(s2[i]);
 			s2[i] = 0;
@@ -29,6 +29,7 @@ static void free_array(char **s2)
 		s2 = 0;
 	}
 }
+
 static int	ft_count_words(char const *str, char c)
 {
 	int	i;
@@ -36,16 +37,17 @@ static int	ft_count_words(char const *str, char c)
 
 	i = 0;
 	count = 0;
-	while ( str[i] !='\0')
+	while (str[i] != '\0')
 	{
-		if (str[i] == c && str[i + 1] != '\0' && str[i + 1] != c )
+		if (str[i] == c && str[i + 1] != '\0' && str[i + 1] != c)
 			count++;
 		i++;	
 	}
-	if(str[0] != c && str[0] !='\0')
+	if (str[0] != c && str[0] != '\0')
 		count++;
 	return (count);
 }
+
 static char	*ft_putword(char *word, char const *s, int i, int word_len)
 {
 	int	j;
@@ -60,7 +62,18 @@ static char	*ft_putword(char *word, char const *s, int i, int word_len)
 	word[j] = '\0';
 	return (word);
 }
+static int ft_word_len(char const *s, char c, int index)
+{
+	int	word_len;
 
+	word_len = 0;
+	while (s[index] && s[index] != c)
+	{
+		index++;
+		word_len++;
+	}
+	return (index);
+}
 static char	**ft_split_words(char const *s, char c, char **s2, int num_words)
 {
 	int	i;
@@ -69,21 +82,19 @@ static char	**ft_split_words(char const *s, char c, char **s2, int num_words)
 
 	i = 0;
 	word = 0;
-	word_len = 0;
 	while (word < num_words)
 	{
 		while (s[i] && s[i] == c)
 			i++;
-		while (s[i] && s[i] != c)
-		{
-			i++;
-			word_len++;
-		}
+
+		word_len = ft_word_len(s, c, i);
+
+		i = i + word_len;
 		s2[word] = (char *)malloc(sizeof(char) * (word_len + 1));
 		if (!s2[word])
 		{
-			free_array(s2);
-			return(0);			
+			free_array (s2);
+			return (0);
 		}
 		ft_putword(s2[word], s, i, word_len);
 		word_len = 0;
